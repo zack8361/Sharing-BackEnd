5543e1ef18a7c05b931c01ffd18772672c7fc611
zack8361 <zack6353@naver.com>
2023-04-13 00:41:25 +0900

    myPage 컨트롤러 수정
    myPage 컨트롤러 수정

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -27,6 +27,7 @@ const showMain = (req, res) => {
 // 마이페이지 쿼리
 const showMypage = (req, res) => {
   try {
+    console.log(req.params.id);
     connection.query(
       `SELECT * FROM user WHERE USER_ID = '${req.params.id}'`,
       (err, data) => {
@@ -35,10 +36,13 @@ const showMypage = (req, res) => {
         const ARTICLE = data;
         const articleCount = ARTICLE.length;
         connection.query(
-          `SELECT * FROM OBJECT O LEFT OUTER JOIN OBJECT_MAP M ON O.OBJECT_TYPE = M.OBJECT_TYPE AND RENT_USER_ID = '${req.params.id}'`,
+          // `SELECT * FROM OBJECT O LEFT OUTER JOIN
+          // OBJECT_MAP M ON O.OBJECT_TYPE = M.OBJECT_TYPE AND RENT_USER_ID = '${req.params.id}'`,
+          `SELECT * FROM OBJECT WHERE RENT_USER_ID = '${req.params.id}'`,
           (err2, data2) => {
             if (err) throw err2;
             const ARTICLE2 = data2;
+
             const articleCount2 = ARTICLE2.length;
             res.status(200).json({
               ARTICLE,
