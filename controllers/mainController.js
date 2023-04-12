d53384e7b14c173f1598793e4c82e0190b04e81c
yulimle <yulim516@naver.com>
2023-04-12 16:48:27 +0900

    230412 mainController 수정

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -7,15 +7,12 @@ const showMain = (req, res) => {
     connection.query('SELECT * FROM OBJECT_MAP', (err, data) => {
       if (err) throw err;
       const ARTICLE = data;
-
-      console.log('######', ARTICLE[0].IMG_SRC, '$$$$$');
       const articleCount = ARTICLE.length;
       connection.query(
         `SELECT USER_NAME FROM USER WHERE USER_ID ='${req.params.id}'`,
         (err2, data2) => {
           if (err2) throw err2;
           const NAME = data2[0];
-          console.log(data);
           res.status(200).json({ ARTICLE, articleCount, NAME });
         },
       );
@@ -30,19 +27,18 @@ const showMain = (req, res) => {
 const showMypage = (req, res) => {
   try {
     connection.query(
-      `SELECT * FROM user WHERE USER_ID = '${req.session.userId}'`,
+      `SELECT * FROM user WHERE USER_ID = '${req.params.id}'`,
       (err, data) => {
         if (err) throw err;
-        console.log(data);
         const ARTICLE = data;
         const articleCount = ARTICLE.length;
         connection.query(
-          `SELECT * FROM OBJECT O LEFT OUTER JOIN OBJECT_MAP M ON O.OBJECT_TYPE = M.OBJECT_TYPE AND RENT_USER_ID = '${req.session.userId}'`,
+          `SELECT * FROM OBJECT O LEFT OUTER JOIN OBJECT_MAP M ON O.OBJECT_TYPE = M.OBJECT_TYPE AND RENT_USER_ID = '${req.params.id}'`,
           (err2, data2) => {
             if (err) throw err2;
             const ARTICLE2 = data2;
             const articleCount2 = ARTICLE2.length;
-            res.render('myPage', {
+            res.status(200).json({
               ARTICLE,
               articleCount,
               ARTICLE2,
